4592ad4398b5be8f709112dff9a25bab8e25f52a
Jesse Chen <jesseminn@gmail.com>
2021-01-02 10:21:18 +0800

    init

---
+++ webpack.config.ts
@@ -0,0 +1,49 @@
+import path from 'path';
+import { Configuration } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+
+// Utils
+import { tsPathsToWebpackAlias } from './webpack.utils';
+
+const config: Configuration = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: {
+        index: path.resolve(__dirname, 'src/index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].bundle.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+        alias: {
+            ...tsPathsToWebpackAlias(),
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            // Prevent index.html get removed on rebuild in watch mode
+            cleanStaleWebpackAssets: false,
+        }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './index.html'),
+        }),
+    ],
+};
+
+export default config;
