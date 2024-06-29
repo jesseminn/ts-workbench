8d3f4aab6ad78ea9c2aead0e8518cc9ce4230918
Jesse <jesseminn@gmail.com>
2024-06-29 15:50:25 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,3 @@
+import { add } from "./src/add";
+
+console.log("hii", add(1, 2));
---
+++ src/subtract/index.test.ts
@@ -0,0 +1,7 @@
+import { subtract } from ".";
+
+describe("subtract", () => {
+    it("should be 1", () => {
+        expect(subtract(2, 1)).toBe(1);
+    });
+});
